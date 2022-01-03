ea7164f5bc7839adfdcd25ef1f885d0399c44407
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2022-01-03 12:52:18 -0300

    process env

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(cors());
 const bodyParser = require("body-parser");
 const port = 3070;
 const isProduction = process.env.NODE_ENV == "production";
+const appUrl = process.env.APP_URL;
 const path = process.cwd();
 const distPath = `${path}/frontend/dist`;
 
@@ -20,7 +21,7 @@ app.get("/", (req, res) => {
     return res.sendFile(`${distPath}/index.html`);
   }
   res.send(
-    "the server is in development mode, to generate the interface run frontend: npm run build and server: npm run start"
+    "the server is running in development mode, to generate the interface run in frontend folder: npm run build and in the root folder : npm run start"
   );
 });
 
@@ -29,5 +30,5 @@ app.get("/api", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on the port::${port}`);
+  console.log(`Server listening on the ${appUrl}:${port}`);
 });

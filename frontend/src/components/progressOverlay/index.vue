33f02e0e0a419b4dfdf702e5e2d8cdb526774550
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2022-02-05 12:53:33 -0300

    play pause logic

--- frontend/src/components/progressOverlay/index.vue
+++ frontend/src/components/progressOverlay/index.vue
@@ -1,28 +1,52 @@
 <template>
   <div class="overlay-progress" v-if="sending">
     <div class="--dialog">
-      <div class="--close-btn">
+      <div class="--close-btn" v-if="show_result" @click="close_result">
         <i class="fas fa-times" />
       </div>
-      <div class="--title">ENVIANDO</div>
-      <div class="--row-btns">
-        <div class="--progress-numbers">
-          {{ current_number_index }}/{{ sending_numbers.length }}
-        </div>
-        <div class="--play-btns">
-          <i class="fas fa-stop" />
-          <i class="fas fa-pause" />
-        </div>
+      <div class="--title">
+        <template v-if="show_result"> FINALIZADO </template>
+        <template v-else>
+          {{ sending_state == "pause" ? "ENVIO PAUSADO" : "ENVIANDO" }}
+        </template>
       </div>
-      <div class="--progressbar">
-        <div
-          class="--progressbar-progress"
-          :style="{
-            width: `${(current_number_index * 100) / sending_numbers.length}%`,
-          }"
-        />
-      </div>
-      <div class="--description">Para editar a mensagem pause o envio</div>
+      <template v-if="show_result"> RESULTADO </template>
+      <template v-else>
+        <div class="--row-btns">
+          <div class="--progress-numbers">
+            {{ current_number_index }}/{{ sending_numbers.length }}
+          </div>
+          <div class="--play-btns">
+            <template v-if="sending_state == 'play'">
+              <a href="#">
+                <i class="fas fa-stop" />
+              </a>
+              <a href="#" @click.prevent="pause">
+                <i class="fas fa-pause" />
+              </a>
+            </template>
+            <template v-else>
+              <a href="#" @click.prevent="play">
+                <i class="fas fa-play" />
+              </a>
+            </template>
+          </div>
+        </div>
+        <div class="--progressbar">
+          <div
+            class="--progressbar-progress"
+            :style="{
+              width: `${
+                (current_number_index * 100) / sending_numbers.length
+              }%`,
+            }"
+          />
+        </div>
+        <div class="--description">
+          Você pode pausar e parar o envio quando quiser clicando nos botões de
+          ação
+        </div>
+      </template>
     </div>
   </div>
 </template>
@@ -35,16 +59,35 @@ export default {
   setup() {
     const store = useStore();
     const sending = computed(() => store.getters["sender/getSending"]);
-    const sending_numbers = computed(
-      () => store.getters["sender/getSendingNumbers"]
-    );
-    const current_number_index = computed(
-      () => store.getters["sender/getCurrentNumberIndex"]
-    );
+    const sending_numbers = computed(() => {
+      return store.getters["sender/getSendingNumbers"];
+    });
+    const current_number_index = computed(() => {
+      return store.getters["sender/getCurrentNumberIndex"];
+    });
+    const show_result = computed(() => store.getters["sender/getShowResult"]);
+    const sending_state = computed(() => {
+      return store.getters["sender/getSendingState"];
+    });
+    const close_result = () => store.dispatch("sender/closeResult");
+    const set_sending_state = (val) => {
+      store.commit("sender/setSendingState", val);
+    };
+    const pause = () => set_sending_state("pause");
+    const play = () => {
+      set_sending_state("play");
+      store.dispatch("sender/sendCurrentNumberMessage");
+    };
+
     return {
       sending,
       sending_numbers,
       current_number_index,
+      show_result,
+      sending_state,
+      close_result,
+      pause,
+      play,
     };
   },
 };
--- frontend/src/store/modules/sender.module.js
+++ frontend/src/store/modules/sender.module.js
@@ -1,6 +1,6 @@
 import { VueApp } from "@/main";
 const validationException = (msg) => ({ type: "warning", msg });
-const sendMessage = async (icon, title, text) => {
+const sendAlert = async (icon, title, text) => {
   VueApp.$swal.fire({ icon, title, text });
 };
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -13,7 +13,9 @@ export default {
       message: "teste 123",
       tab: 0,
       sending: false,
+      show_result: false,
       current_number_index: 0,
+      sending_state: "play",
     };
   },
   getters: {
@@ -23,44 +25,53 @@ export default {
     getSending: (state) => state.sending,
     getSendingNumbers: (state) => state.numbers.split(","),
     getCurrentNumberIndex: (state) => state.current_number_index,
+    getShowResult: (state) => state.show_result,
+    getSendingState: (state) => state.sending_state,
   },
   mutations: {
     setNumbers: (state, payload) => (state.numbers = payload),
     setTab: (state, payload) => (state.tab = payload),
     setMessage: (state, payload) => (state.message = payload),
     setSending: (state, payload) => (state.sending = payload),
-    setCurrentNumberIndex: (state, payload) =>
-      (state.current_number_index = payload),
+    setCurrentNumberIndex: (state, payload) => {
+      state.current_number_index = payload;
+    },
+    setShowResult: (state, payload) => (state.show_result = payload),
+    setSendingState: (state, payload) => (state.sending_state = payload),
   },
   actions: {
+    closeResult({ commit }) {
+      commit("setSending", false);
+      commit("setShowResult", false);
+    },
     finishSending({ commit }) {
       commit("setCurrentNumberIndex", 0);
-      commit("setSending", false);
-      return sendMessage(
-        "success",
-        "Finalizado !",
-        "Mensagens enviadas com sucesso"
-      ).then(() => {
-        commit("setMessage", "");
-        commit("setNumbers", "");
-      });
+      commit("setShowResult", true);
     },
     async sendCurrentMessage({ dispatch, commit, getters }, number) {
       commit("setSending", true);
       let index = getters.getCurrentNumberIndex;
-
       if (index == getters.getSendingNumbers.length) {
         return dispatch("finishSending");
       } else {
-        console.log("Enviando", number, getters.getMessage);
-        await sleep(1500); // emula demora envio
-        commit("setCurrentNumberIndex", index + 1);
-        dispatch("sendCurrentNumberMessage");
+        await dispatch("sendEngineMessage");
+        let sending_state = getters.getSendingState;
+        console.log(sending_state);
+        if (sending_state == "play") {
+          commit("setCurrentNumberIndex", index + 1);
+          console.log("ENVIANDO", number, getters.getMessage);
+          dispatch("sendCurrentNumberMessage");
+        }
       }
     },
-    sendCurrentNumberMessage({ dispatch, getters }) {
+    async sendEngineMessage() {
+      // emula demora envio
+      return sleep(1000);
+    },
+    sendCurrentNumberMessage({ dispatch, commit, getters }) {
       let index = getters.getCurrentNumberIndex;
       let number = getters.getSendingNumbers[index];
+      commit("setShowResult", false);
       dispatch("sendCurrentMessage", number);
     },
     sendMessageFromNumber({ dispatch, state }) {
@@ -80,12 +91,11 @@ export default {
         0: () => dispatch("sendMessageFromNumber"),
         1: () => dispatch("sendMessageFromSheet"),
       };
-
       try {
         actions[state.tab] && actions[state.tab]();
       } catch (error) {
         if (error?.type) {
-          sendMessage(error.type, "Erro", error.msg);
+          sendAlert(error.type, "Erro", error.msg);
         }
       }
     },

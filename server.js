1a7d760c8988b387a7ab89a5635c8370457fcb9f
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2022-03-24 14:15:57 -0300

    cors fix

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ require('module-alias').addAliases({
 });
 
 const SocketIo = require('socket.io');
-const { Auth } = require('@src/middlewares/auth.middleware');
+const { Auth, checkUser } = require('@src/middlewares/auth.middleware');
 const wppEngine = require('@src/engines/wpp.engine');
 const DBConn = require('@src/utils/connector.util');
+const debug = require('console-development');
 const { app, http } = require('./bootstrap');
 
 DBConn.connect();
@@ -16,22 +17,27 @@ DBConn.connect();
 const io = SocketIo(http, {
   allowEIO3: true,
   cors: {
-    origin: true,
-    credentials: true,
+    origin: '*',
   },
 });
 
-io.sockets.on('connection', (socket) => {
-  socket.emit('connected', { id: socket.id });
+io.sockets.on('connection', async (socket) => {
+  const { username, password } = socket.handshake.query;
+  const user = await checkUser(username, password);
+  if (user) {
+    socket.emit('connected', { id: socket.id });
 
-  socket.on('start-engine', (code) => {
-    wppEngine.initClientSession(code, socket);
-  });
+    socket.on('start-engine', (code) => {
+      wppEngine.initClientSession(code, socket);
+    });
 
-  socket.on('message', async (params) => {
-    const result = await wppEngine.handleSendMessage(params, socket);
-    return result;
-  });
+    socket.on('message', async (params) => {
+      const result = await wppEngine.handleSendMessage(params, socket);
+      return result;
+    });
+  } else {
+    socket.emit('error', { message: 'Invalid credentials' });
+  }
 });
 
 app.use('/sessions', require('./src/routes/sessions.route'));

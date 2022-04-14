897eeca6a15ab633d133e46cd437c8b82094ac30
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2022-04-14 08:36:36 -0300

    remove cors

--- bootstrap.js
+++ bootstrap.js
@@ -5,13 +5,6 @@ const http = require('http').createServer(app);
 const cors = require('cors');
 
 app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  next();
-});
 
 const bodyParser = require('body-parser');
 const debug = require('console-development');
@@ -26,10 +19,6 @@ app.post('/postback-mockup', async (req, res) => {
   const params = req.body;
   debug.log('postback mockup', params);
   res.send(params);
-});
-
-http.prependListener('request', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
 });
 
 const port = process.env.PORT || 3000;
